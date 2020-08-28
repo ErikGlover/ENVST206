324cafa4d0abbad2bba67144c8c0a9344aa2dd06
Erik Glover <erikglover@eriks-mbp-2.hamilton.edu>
2020-08-28 11:07:39 -0400

    description of changes

--- activity1/activity1_script.r
+++ activity1/activity1_script.r
@@ -1,2 +1,12 @@
 # Activity 1
 print("hello R world")
+#45 divided by 3
+45/3
+#name my number
+a <- 1483
+#multiply my number by 33
+a*33
+#make a vector of numbers
+b<-c(27,493,3965)
+#divide my numbers by 52
+b/52
