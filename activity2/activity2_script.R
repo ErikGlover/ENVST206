c9e28bd1bcdd6ff6343c81420247752078a975b8
Erik Glover <erikglover@eriks-mbp-2.hamilton.edu>
2020-09-10 08:44:49 -0400

    description of changes

---
+++ activity2/activity2_script.R
@@ -0,0 +1,12 @@
+#activity 2
+
+heights <- c(3,2,3)
+
+
+datW <- read.csv("/Users/erikglover/Documents/Environmental Data/a02/noaa2011124.csv")
+
+#make a vector of tree heights in meters
+heights <- c(30,41,20,22)
+#convert to cm
+heights_cm <- heights*100
+heights_cm
