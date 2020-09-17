59523fa6288aa1dd81d4d906b95eb8c08c446856
Erik Glover <erikglover@eriks-mbp-2.hamilton.edu>
2020-09-17 15:30:02 -0400

    changes

--- activity3/activity3_script.r
+++ activity3/activity3_script.r
@@ -1,5 +1,16 @@
 #activity2
 
+ch4 <- read.csv("/Users/erikglover/Documents/Environmental Data/a03/lemming_herbivory.csv")
+
+ch4$herbivory <- as.factor(ch4$herbivory)
 
 
+plot(ch4$CH4_Flux ~ ch4$herbivory)
 
+shapiro.test(ch4$CH4_Flux[ch4$herbivory == "Ex"])
+shapiro.test(ch4$CH4_Flux[ch4$herbivory == "Ctl"])
+
+#dependent variable ~ independent variable
+bartlett.test(ch4$CH4_Flux ~ ch4$herbivory)
+
+t.test(ch4$CH4_Flux ~ ch4$herbivory)
